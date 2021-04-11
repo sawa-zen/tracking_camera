a588294e2b6d55df1f10428993b1fa7e212fcd68
sawa-zen <sawadasuiren@gmail.com>
2021-04-12 01:09:44 +0900

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const raspi = require('raspi');
 var PWM = require('raspi-pwm').PWM;
 
-
 var minRate = 0.025
 var maxRate = 0.12
 var rateDiff = maxRate - minRate
@@ -15,8 +14,8 @@ var minDegree = 60
 var maxDegree = 180
 var intervalTimespan = 20
 raspi.init(() => {
-    var pwm1 = new PWM('GPIO18');
-    var pwm2 = new PWM('GPIO19');
+    var servo1 = new SG90('GPIO18', 90)
+    var servo2 = new SG90('GPIO19', 90)
     setInterval(() => {
         degree += addNum
         console.info(degree, addNum)
@@ -28,19 +27,46 @@ raspi.init(() => {
             degree = minDegree
             addNum = -addNum
         }
-        const rate1 = convertDegreeToRate(degree)
-        pwm1.write(rate1)
-        const rate2 = convertDegreeToRate(degree)
-        pwm2.write(rate2)
+        servo1.setDegree(degree)
+        servo2.setDegree(degree)
     }, intervalTimespan)
 
     process.stdin.resume();
     process.on('SIGINT', function() {
-        const rate1 = convertDegreeToRate(90)
-        pwm1.write(rate1)
-        const rate2 = convertDegreeToRate(90)
-        pwm2.write(rate2)
-        //終了処理…
-        process.exit();
+        servo1.reset()
+        servo2.reset()
+        process.exit()
     });
 });
+
+class SG90 {
+    static MIN_DEGREE = 0
+    static MAX_DEGREE = 180
+
+    degree = 0
+
+    constructor(pin, defaultDegree) {
+        this.pin = pin
+        this.defaultDegree = defaultDegree
+        this.pwm = new PWM(pin)
+        this.setDegree(defaultDegree)
+    }
+
+    setDegree(degree) {
+        if (degree > SG90.MAX_DEGREE) {
+            console.warn(`${degree}度は最大角度の180度を超過しています`)
+            degree = SG90.MAX_DEGREE
+        } else if (degree < SG90.MIN_DEGREE) {
+            console.warn(`${degree}度は最小角度の0度を超過しています`)
+            degree = SG90.MIN_DEGREE
+        }
+
+        this.degree = degree
+        const rate = convertDegreeToRate(degree)
+        this.pwm.write(rate)
+    }
+
+    reset() {
+        this.setDegree(this.defaultDegree)
+    }
+}
